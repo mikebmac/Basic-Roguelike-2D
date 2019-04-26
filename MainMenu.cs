7dad4a4cf2ee16a02ffbbde85439288c581b3bc8
Michael MacKay <mbm1@ualberta.ca>
2019-04-25 18:25:59 -0600

    Initial commit

---
+++ MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsMenu;
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenSettings() {
+        settingsMenu.SetActive(true);
+    }
+
+    public void CloseSettings() {
+        settingsMenu.SetActive(false);
+    }
+
+    public void ExitGame() {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+}

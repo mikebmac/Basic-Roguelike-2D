8a4fbd4d55ad5f48ed0f3058677db94024e48c80
Michael MacKay <mbm1@ualberta.ca>
2019-04-26 11:42:31 -0600

    0.1

---
+++ Scripts/PlayerContoller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject weapon;
+
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float _movementSpeed = 5f;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        // Weapon Angle
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = Utilities.AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        weapon.transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle));
+
+        // Show Weapon
+        if(Input.GetMouseButton(0)) {
+            weapon.SetActive(true);
+        } else {
+            weapon.SetActive(false);
+        }
+
+        // Movement
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (move.magnitude > 1.0f) {
+            move = move.normalized;
+        }
+        rb.velocity = move * _movementSpeed;
+    }
+}
---
+++ Scripts/Utilities.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+     }
+}
